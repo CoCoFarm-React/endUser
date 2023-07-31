915902e00b8793737c630bbdc55788c6ba2bb5d4
joungjaehyun <joungjaehyun1008@gmail.com>
2023-07-31 11:48:49 +0900

    route

--- src/components/farmers/FarmerSigninComponent.js
+++ src/components/farmers/FarmerSigninComponent.js
@@ -2,6 +2,17 @@ const FarmerSiginComponent = () => {
     return (  
         <div>
             FarmerSiginComponent~~
+            <div>
+                Id
+                <input type="text" name="email"></input>
+            </div>
+            <div>
+                pw
+                <input type="text" name="pw"></input>
+            </div>
+            <div>
+                <button>Sign In</button>
+            </div>
         </div>
     );
 }
