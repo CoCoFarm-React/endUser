37869836c3160c0a1d8bbe51ee6123ae36b2c47f
beomsu1 <128343741+hiYewonMoon1@users.noreply.github.com>
2023-07-31 11:49:03 +0900

    0731

---
+++ src/pages/consumer/SignInPage.js
@@ -0,0 +1,18 @@
+
+const SignInPage = () => {
+    return (  
+        <div>
+            <div className="m-1">
+                <input type="texet" className="bg-purple-100"></input>
+            </div>
+            <div className="m-1">
+                <input type="texet" className="bg-purple-100"></input>
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </div>
+    );
+}
+ 
+export default SignInPage;
