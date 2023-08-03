8da26636bfc82ab3d9222e980840c6700271489c
tarkIlJun <tack89826@gmail.com>
2023-08-03 12:29:40 +0900

    작업내용

---
+++ src/pages/consumer/ConsumerPage.js
@@ -0,0 +1,10 @@
+
+const ConsumerPage = () => {
+    return ( 
+        <div>
+            <h1>ConsumerPage</h1>
+        </div>
+     );
+}
+ 
+export default ConsumerPage;
--- src/routers/root.js
+++ src/routers/root.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from"react-router-dom";
 import App from "../App";
 import FarmerSigninPage from "../pages/farmers/FarmerSigninPage";
 import SignInPage from "../pages/common/SignInPage";
+import ConsumerPage from "../pages/consumer/ConsumerPage";
 
 const router = createBrowserRouter([
     {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
     {
         path:"farmersignin",
         element: <FarmerSigninPage></FarmerSigninPage>
+    },
+    {
+        path:"ConsumerPage",
+        element: <ConsumerPage></ConsumerPage>
     }
 ])
 
