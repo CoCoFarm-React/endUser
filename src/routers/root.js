09b8349ec1f20db9b2ccced991833a6990bb15cd
soohosuh <128343379+soohosuh@users.noreply.github.com>
2023-08-03 12:19:58 +0900

    기본설정

--- src/routers/root.js
+++ src/routers/root.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from"react-router-dom";
 import App from "../App";
 import FarmerSigninPage from "../pages/farmers/FarmerSigninPage";
 import SignInPage from "../pages/common/SignInPage";
+import BoardPage from "../pages/consumer/BoardPage";
 
 const router = createBrowserRouter([
     {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
     {
         path:"farmersignin",
         element: <FarmerSigninPage></FarmerSigninPage>
+    },
+    {
+        path:"BoardPage",
+        element: <BoardPage></BoardPage>
     }
 ])
 
