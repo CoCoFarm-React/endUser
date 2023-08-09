4d0f0e1a25eb798255be0827ac0497cb0ebdf99f
soohosuh <128343379+soohosuh@users.noreply.github.com>
2023-08-09 10:39:26 +0900

    0809

--- src/api/FarmerAPI.js
+++ src/api/FarmerAPI.js
@@ -1,48 +1,48 @@
-import axios from "axios"
-import { createSearchParams } from "react-router-dom"
+// import axios from "axios"
+// import { createSearchParams } from "react-router-dom"
 
-export const getList = async (queryObj) => {
+// export const getList = async (queryObj) => {
 
-  const queryString = createSearchParams(queryObj).toString();
+//   const queryString = createSearchParams(queryObj).toString();
 
-  const res = await axios.get(`http://localhost:8080/api/board/list?${queryString}`)
+//   const res = await axios.get(`http://localhost:8080/api/board/list?${queryString}`)
 
-  return res.data
-
-}
-
-export const getOne = async (bno) => {
-
-  const res = await axios.get(`http://localhost:8080/api/board/${bno}`)
-
-  return res.data
-}
-
-// export const getFarmerList = async (queryObj) =>{
-
-//   const res = await axios.get(`http://192.168.0.11:8080/api/admin/farmer`)
 //   return res.data
-  
+
 // }
 
+// export const getOne = async (bno) => {
 
-// 서버연동
+//   const res = await axios.get(`http://localhost:8080/api/board/${bno}`)
+
+//   return res.data
+// }
+
+// // export const getFarmerList = async (queryObj) =>{
+
+// //   const res = await axios.get(`http://192.168.0.11:8080/api/admin/farmer`)
+// //   return res.data
+  
+// // }
+
+
+// // 서버연동
+// // export const getConsumerList = async (queryObj) =>{
+
+  
+// //   const res = await axios.get(`http://192.168.0.11:8080/api/admin/consumer`)
+
+// //   return res.data
+  
+// // }
+
+// // 로컬 테스트용
 // export const getConsumerList = async (queryObj) =>{
 
   
-//   const res = await axios.get(`http://192.168.0.11:8080/api/admin/consumer`)
+//   const res = await axios.get(`http://localhost:8080/api/admin/consumer`)
 
 //   return res.data
   
 // }
-
-// 로컬 테스트용
-export const getConsumerList = async (queryObj) =>{
-
-  
-  const res = await axios.get(`http://localhost:8080/api/admin/consumer`)
-
-  return res.data
-  
-}
 
--- src/layout/consumernav/SampleNav.js
+++ src/layout/consumernav/SampleNav.js
@@ -8,7 +8,7 @@ const SampleNav = () => {
                 <Link to="/">COCOFARM</Link>
             </div>
             <div  className='font-size-16px font-serif'>
-                <Link to="/consumer/about">ABOUT</Link>
+                <Link to="/about">ABOUT</Link>
             </div>
             <div  className='font-size-16px font-serif'>
                 <Link to="/farmer/home">FARMER</Link>
