3f218676cc6440edd1bd3d39790cb0c8040a5065
chanunu90 <128343652+chanunu90@users.noreply.github.com>
2023-08-18 10:04:23 +0900

    0818

--- src/util/KakaoMap.js
+++ src/util/KakaoMap.js
@@ -3,26 +3,13 @@ import Modal from "./Modal";
 
 const {kakao} = window
 
-const initState = {
-    address:''
-}
-
-const KakaoMap = () => {
-
-    const [selectedAddress, setSelectedAddress] = useState(initState)
-
-    const getAddress = (param) => {
-
-        selectedAddress.address = param.address
-        setSelectedAddress({...selectedAddress})
-
-    }
+const KakaoMap = ({address}) => {
 
     useEffect(() => {
         const container = document.getElementById('map')
         const options = {
             center: new kakao.maps.LatLng(33.450701, 126.570667),
-            level: 3
+            level: 5
         }
         const map = new kakao.maps.Map(container, options)
 
@@ -30,7 +17,7 @@ const KakaoMap = () => {
 
         
         // 주소로 좌표를 검색합니다.
-        geocoder.addressSearch(selectedAddress.address ? selectedAddress.address : '한천로713' , function (result, status) {
+        geocoder.addressSearch(address ? address : '카카오 분당' , function (result, status) {
 
         // 정상적으로 검색이 완료됐으면 
         if (status === kakao.maps.services.Status.OK) {
@@ -45,24 +32,36 @@ const KakaoMap = () => {
     
           // 인포윈도우로 장소에 대한 설명을 표시합니다
           var infowindow = new kakao.maps.InfoWindow({
-            content: '<div style="width:150px;color:red;text-align:center;padding:6px 0;">현재 위치</div>'
+            content: '<div id="infoBox" style="width:150px;color:red;text-align:center;padding:6px 0;"></div>',
+            // disableAutoPan: true
           });
+
           infowindow.open(map, marker);
     
           // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
           map.setCenter(coords);
+
+          // 상위 요소 찾아서 display: none 적용
+          var infoBox = document.getElementById('infoBox');
+          if (infoBox) {
+            var parentElement = infoBox.parentNode;
+            if (parentElement) {
+              parentElement.parentNode.style.display = 'none';
+            }
+          }
+
         }
       })
 
-    } , [selectedAddress ])
+
+
+
+    } , [address])
+
 
     return (
-        <div className="relative">
-            KakaoMap
-            <div id="map" className="border-2 w-[500px] h-[500px]"></div>
-            <div className="w-[500px] h-[500px] absolute right-0 top-10 z-10">
-                <Modal getAddress={getAddress}></Modal>
-            </div>
+        <div >
+            <div id="map" className="border-2 w-[500px] h-[250px]"></div>
         </div>
     );
 }
